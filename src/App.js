990ddfa3069fbd3c40bd15fe09363d374d02ae65
MaxBrych <max.brych03@gmail.com>
2023-12-26 14:09:05 +0100

    created section components and OGP meta tags

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <head>
+        <meta name="og:title" content="Little Lemon Restaurant" />
+        <meta
+          name="og:description"
+          content="Get the best food in town at Little Lemon Restaurant. Open Monday to Friday, 9 to 5, in the Great Lake area."
+        />
+        <meta
+          name="og:image"
+          content="https://cdn.discordapp.com/attachments/911669935363752026/1189192695827144744/llr_image.jpg?ex=659d4500&is=658ad000&hm=c68bd6a0f884fd8a0816ba559aab02f833c41e7082c0c756f492b17336d246d7&"
+        />{" "}
+      </head>
+
+      <main></main>
+      <footer></footer>
+    </>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <>
+      <svg></svg>
+      <ul>
+        <li></li>
+      </ul>
+    </>
+  );
+}
