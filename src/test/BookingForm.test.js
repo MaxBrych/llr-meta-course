f6f1a8c9fd643994d737387d8a4fdd8d44099ef6
MaxBrych <max.brych03@gmail.com>
2023-12-27 19:12:30 +0100

    add tests

--- src/test/BookingForm.test.js
+++ src/test/BookingForm.test.js
@@ -1,17 +1,57 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import BookingForm from "../components/BookingForm";
 
+beforeAll(() => {
+  window.fetchAPI = jest.fn(() => Promise.resolve(["12:00", "13:00", "18:00"]));
+});
+
 describe("BookingForm", () => {
+  test("updates available times when date is selected", async () => {
+    // Mock functions
+    const mockSetDate = jest.fn();
+    const mockDispatchAvailableTimes = jest.fn();
+
+    // Mock the fetchAPI function to return specific times for a given date
+    window.fetchAPI = jest.fn((date) => {
+      if (date === "2023-12-01") {
+        return Promise.resolve(["17:00", "19:00", "21:00"]);
+      }
+      return Promise.resolve([]);
+    });
+
+    render(
+      <BookingForm
+        date=""
+        setDate={mockSetDate}
+        time=""
+        setTime={() => {}}
+        guests={1}
+        setGuests={() => {}}
+        occasion=""
+        setOccasion={() => {}}
+        availableTimes={[]}
+        dispatchAvailableTimes={mockDispatchAvailableTimes}
+      />
+    );
+
+    fireEvent.change(screen.getByLabelText(/choose date/i), {
+      target: { value: "2023-12-01" },
+    });
+
+    await waitFor(() => {
+      expect(window.fetchAPI).toHaveBeenCalledWith("2023-12-01");
+      expect(mockDispatchAvailableTimes).toHaveBeenCalled();
+    });
+  });
+
   test("allows the user to submit a booking", () => {
-    // Mock the props
+    // Mock functions
     const mockSetDate = jest.fn();
     const mockSetTime = jest.fn();
     const mockSetGuests = jest.fn();
     const mockSetOccasion = jest.fn();
-    const mockDispatchAvailableTimes = jest.fn();
 
-    // Render the component with mock functions
     render(
       <BookingForm
         date=""
@@ -23,7 +63,7 @@ describe("BookingForm", () => {
         occasion=""
         setOccasion={mockSetOccasion}
         availableTimes={["17:00", "18:00", "19:00"]}
-        dispatchAvailableTimes={mockDispatchAvailableTimes}
+        dispatchAvailableTimes={() => {}}
       />
     );
 
@@ -51,6 +91,5 @@ describe("BookingForm", () => {
     expect(mockSetTime).toHaveBeenCalledWith("18:00");
     expect(mockSetGuests).toHaveBeenCalledWith(4);
     expect(mockSetOccasion).toHaveBeenCalledWith("Birthday");
-    expect(mockDispatchAvailableTimes).toHaveBeenCalled();
   });
 });
