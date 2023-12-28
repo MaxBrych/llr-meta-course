307c7acb5979d70caef83c202f360ad5275428e6
MaxBrych <max.brych03@gmail.com>
2023-12-28 20:51:32 +0100

    add content

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,18 +5,20 @@ import { Box } from "@chakra-ui/react";
 
 export default function NavBar() {
   return (
-    <nav>
-      <Link to="/">
-        <LogoDark />
-      </Link>
-      <nav class="links">
-        <Link class="link" to="/">
-          Home
+    <>
+      <nav>
+        <Link to="/">
+          <LogoDark />
         </Link>
-        <Link class="link" to="/Booking">
-          Booking
-        </Link>
+        <nav class="links">
+          <Link class="link" to="/">
+            Home
+          </Link>
+          <Link class="link" to="/Booking">
+            Booking
+          </Link>
+        </nav>
       </nav>
-    </nav>
+    </>
   );
 }
