92c75d1ca07717242dec49c6eebdde77bc7b44c1
MaxBrych <max.brych03@gmail.com>
2024-01-01 21:54:54 +0100

    restructure footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "./lib/LogoLight";
+import { Link } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 
 export default function Footer() {
@@ -9,33 +10,29 @@ export default function Footer() {
         <div class="footer-col">
           {" "}
           <Logo />{" "}
-          <div class="contact-info">
-            <div class="info">
-              {" "}
-              <h6>+49 01231595</h6>{" "}
-            </div>
-            <div class="info">
-              <h6>little.lemon@gmail.com</h6>
-            </div>
+        </div>
+
+        <div class="footer-col-text">
+          <h6>E-Mail</h6>
+          <div class="info">
+            <h6>little.lemon@gmail.com</h6>
+          </div>
+        </div>
+        <div class="footer-col-text">
+          <h6>Telephone</h6>
+          <div class="info">
+            {" "}
+            <h6>+49 01231595</h6>{" "}
           </div>
         </div>
         <div class="footer-col-text">
           <h6>Navigation</h6>
-          <a href="">idk</a> <a href="">idk</a>
-          <a href="">idk</a> <a href="">idk</a>
-          <a href="">idk</a>
-        </div>
-        <div class="footer-col-text">
-          <h6>Navigation</h6>
-          <a href="">idk</a> <a href="">idk</a>
-          <a href="">idk</a> <a href="">idk</a>
-          <a href="">idk</a>
-        </div>
-        <div class="footer-col-text">
-          <h6>Navigation</h6>
-          <a href="">idk</a> <a href="">idk</a>
-          <a href="">idk</a> <a href="">idk</a>
-          <a href="">idk</a>
+          <Link class="link" to="/">
+            Home
+          </Link>
+          <Link class="link" to="/Booking">
+            Booking
+          </Link>
         </div>
       </div>
     </div>
