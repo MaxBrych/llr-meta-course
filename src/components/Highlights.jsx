f0fffff7fdbcdb399382d5e9e96334b0cfef5b75
MaxBrych <max.brych03@gmail.com>
2024-01-19 16:55:55 +0100

    new image urls

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -11,7 +11,7 @@ const highlights = [
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, ",
     price: 12,
     image:
-      "https://s3-alpha-sig.figma.com/img/743e/73c0/9d4d70c3d21be03656beaabc90c50931?Expires=1704672000&Signature=Tm-J57PV6fIRVrO5KdX7QZaEcx7gJOo-gPfFNuAUfPdogm9ZtKKb4jJJV85A9A2zJY5L85gP13vELamz5gvFFvtercZ6fR1xWAlDMSlg2sMq0yjNqfboHc0gsknOmiNXpx6keQLvDCUpuV~GN0Vuxb0ER-e3zI1xuubvKWrhLTkbu5bO1Qb30YFSx5VOZaYfooYI-lnOPw6tOu9aNOOL9zAGMxkvz8VDpKkRJ-hO~8kQs7HYNFEbCS9MEcsxzIZKSirDg0WTJ97ny5w36Dwm7AxO5hOTQcfB-7qxQBuMKiymkflkMXWcbc-Be6Knu4qD2NGE~f8E2tD4stTFyKWfXQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://cdn.discordapp.com/attachments/921375530186579979/1197931809216938174/image_1.jpg?ex=65bd0fef&is=65aa9aef&hm=db152ada42d04b4846b32d95487b584a846f6075fb2915dfed0b314613f634ea&",
   },
   {
     id: 2,
@@ -20,7 +20,7 @@ const highlights = [
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, ",
     price: 12,
     image:
-      "https://s3-alpha-sig.figma.com/img/9fda/4f2e/ecbe1e39100ecc3e7caf8e2023ee9e77?Expires=1704672000&Signature=Qwoe7isl1lK8tShLMIQ9l4ZKw78266S9MVjApUMNyklyhIPJpQntfJrWmV1Qh2KJogz9joibXFJthFHgpnWra1cAelphft0EZbauVbWMTsgB3P9KDhIATnluP6bMeh9Y9KzQ9Pk48mfz6TJcQtMrKiv3Q33Yno2sn-zZhYmYRDOA0U8fcyWXRKYJkQ9M1W8KmRrg1THSGCFJ-2RRWsFBFXY9jJEhTQ0jHxMjxuBX3W1ja~M8hA7fd7JaErEubbbq3Y3Y5EFRzgpH46MtJuXs22E2dr-uorwxNtS3rKBQH99t1t1aB~iR4QU0DY~2Pmjm0lD52f1NpWWmzIH-wQ5ZZw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://cdn.discordapp.com/attachments/921375530186579979/1197931809833488484/image_8.jpg?ex=65bd0fef&is=65aa9aef&hm=04f5b61935255bb2e789c69587066c29d5eac9264a4ed685b34b12de91ec5135&",
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ const highlights = [
       "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, ",
     price: 12,
     image:
-      "https://s3-alpha-sig.figma.com/img/7aef/3e7e/2f9ce2eeda86634395f8184b2a2ee293?Expires=1704672000&Signature=jICxzdcAHBWflKWbwop-weYZ3BmcclBRLlwZrYTsgsEEgfw5cenQBVyNZaomBkH0AMJtukBFuG0TNvialdPjVeQcTdPV7dtCx6TOM6oWoa3jmY4zpT85NDiv75bjhE~5Dkv76-w7IVYBEJqrpb41XROhmZtgcOjVEMOrE9VwqHR-0b2HeCWZWm2wFt1CFOTCmMirjOE0x22pfUqr2eBn1igwr3RLB4xkqzUbMLa5IKarwv4xZf2TXl6kKRKWuENvmXHT6A0SHi2g23Dapi2j0uq8IkYgaCxHj6PTIGwl7ODH7qRWI4A~sVZSEFYddnPTIKNNDEJylUoE-h77zUsYPg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+      "https://cdn.discordapp.com/attachments/921375530186579979/1197931807782473788/image_5.jpg?ex=65bd0fef&is=65aa9aef&hm=6bea75a07dd12be140321c115c1e5c8aff6848f977bee0309b531de2fb05f339&",
   },
 ];
 
